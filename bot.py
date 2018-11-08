33ceddde3bc7242e47baa5ae19e46c453110fefb
Xenzai <37379722+Raptor3500@users.noreply.github.com>
2018-11-08 18:34:52 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -39,41 +39,6 @@ async def say(ctx, *args):
         await asyncio.sleep(1)
         await bot.say(mesg)
         print (ctx.message.author.id + " or " + ctx.message.author.name + " made me say '{}'".format(mesg))
-        
-@bot.command(pass_context=True)
-async def invite(ctx):
-  """Invite Me"""
-  await bot.say("here's my invite link")
-  await bot.say("https://discordapp.com/api/oauth2/authorize?client_id=493204973685964830&permissions=8&scope=bot")
-  
-@bot.command(pass_context=True)
-async def help(ctx):
-  embed = discord.Embed(name='help', description=None, color=0x426ef4)
-  embed.set_author(name=ctx.message.author.name)
-  embed.add_field(name='owner', value='not finished', inline=False)
-  embed.add_field(name='cmds', value='List of commands (so far)', inline=False)
-  
-  await bot.say(embed=embed)
-  
-@bot.command(pass_context=True)
-async def owner(ctx):
-  embed = discord.Embed(name='owner', description=None, color=0x426ef4)
-  embed.set_author(name=ctx.message.author.name)
-  embed.add_field(name='playing', value='Sets my playing status', inline=False)
-  embed.add_field(name='watching', value='Sets my watching status', inline=False)
-  embed.add_field(name='listening', value='Sets my listening status', inline=False)
-  
-  await bot.say(embed=embed)
-  
-@bot.command(pass_context=True)
-async def cmds(ctx):
-  embed = discord.Embed(name='cmds', description=None, color=0x426ef4)
-  embed.set_author(name=ctx.message.author.name)
-  embed.add_field(name='say', value='says your message', inline=False)
-  embed.add_field(name='invite', value='Invite Me', inline=False)
-  
-  
-  await bot.say(embed=embed)
 
 if __name__ == "__main__":
     for extension in startup_extensions:
@@ -104,20 +69,21 @@ async def listening(ctx, *args):
     
 @bot.command(pass_context=True)
 async def info(ctx, user: discord.Member):
+  if ctx.message.author.id in ownerID:
   
-  embed = discord.Embed(title="{}'s info".format(user.name), description='Here is what I could find:', color=ctx.message.author.color)
-  embed.add_field(name='Name', value='{}'.format(user.name))
-  embed.add_field(name='ID', value='{}'.format(user.id), inline=True)
-  embed.add_field(name='Status', value='{}'.format(user.status), inline=True)
-  embed.add_field(name='Highest Role', value='<@&{}>'.format(user.top_role.id), inline=True)
-  embed.add_field(name='Joined at', value='{:%d/%h/%y at %H:%M}'.format(user.joined_at), inline=True)
-  embed.add_field(name='Created at', value='{:%d/%h/%y at %H:%M}'.format(user.created_at), inline=True)
-  embed.add_field(name='Discriminator', value='{}'.format(user.discriminator), inline=True)
-  embed.add_field(name='Playing', value='{}'.format(user.game))
-  embed.set_footer(text="{}'s Info".format(user.name), icon_url='{}'.format(user.avatar_url))
-  embed.set_thumbnail(url=user.avatar_url)
+    embed = discord.Embed(title="{}'s info".format(user.name), description='Here is what I could find:', color=ctx.message.author.color)
+    embed.add_field(name='Name', value='{}'.format(user.name))
+    embed.add_field(name='ID', value='{}'.format(user.id), inline=True)
+    embed.add_field(name='Status', value='{}'.format(user.status), inline=True)
+    embed.add_field(name='Highest Role', value='<@&{}>'.format(user.top_role.id), inline=True)
+    embed.add_field(name='Joined at', value='{:%d/%h/%y at %H:%M}'.format(user.joined_at), inline=True)
+    embed.add_field(name='Created at', value='{:%d/%h/%y at %H:%M}'.format(user.created_at), inline=True)
+    embed.add_field(name='Discriminator', value='{}'.format(user.discriminator), inline=True)
+    embed.add_field(name='Playing', value='{}'.format(user.game))
+    embed.set_footer(text="{}'s Info".format(user.name), icon_url='{}'.format(user.avatar_url))
+    embed.set_thumbnail(url=user.avatar_url)
   
-  await bot.say(embed=embed)
+    await bot.say(embed=embed)
   
 @bot.command()
 async def rate(str : str):
@@ -131,16 +97,12 @@ async def rate(str : str):
 @bot.command(pass_context=True)
 async def prune(ctx, number, *args):
   if ctx.message.author.id in ownerID:
-    channel = messages[0].channel
-    mesg = ' '.join(args)
+    channel = messages[0].channel)
     mgs = [] #Empty list to put all the messages in the log
     number = int(number) #Converting the amount of messages to delete to an integer
     async for x in bot.logs_from(ctx.message.channel, limit = number):
         mgs.append(x)
     await bot.delete_messages(mgs)
-    await asyncio.sleep(1)
-    await bot.say(channel, "I have deleted" + (mesg) + "messages")
-    await bot.delete_messages("I have deleted" + (mesg) + "messages")
     
 @bot.command()
 async def choose(str : str, *args):
