59898ac51796eeeb91ed899b0c5095854c7d34b1
Xenzai <37379722+Raptor3500@users.noreply.github.com>
2018-11-08 22:30:55 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,6 +6,10 @@ import os
 import random
 import youtube_dl
 
+startup_extensions = [
+  'cogs.message',
+]
+
 bot = commands.Bot(command_prefix='natsuki ')
 bot.remove_command('help')
 ownerID = "274298631517896704"
